6388ca86c134124ae160eab42216fed5cac54cf4
Fran Martinez <fran.martinez.torres@gmail.com>
2021-02-18 13:07:05 +0100

    fichero bbdd Profesionales

--- mainProfessional.ts
+++ mainProfessional.ts
@@ -1,7 +1,16 @@
 import {Professional} from "./claseProfessional"
 
-let tarantino:Professional = new Professional("Quentin Tarantino",54,"Accion",80,184,"Black","Brown","Caucasian",false,"Us citizen",2,"Director")
-let georgeLucas:Professional = new Professional("George Lucas",75,"Sci-Fi",90,184,"Black","Brown","Caucasian",true,"Us citizen",1,"Director")
-let jonFavreau:Professional = new Professional("Jon Favreau",54,"Action",80,184,"Black","Brown","Caucasian",false,"Us citizen",2,"Director")
-
+let tarantino:Professional = new Professional("Quentin Tarantino",54,"male",80,184,"Black","Brown","Caucasian",false,"Us citizen",2,"Director")
+let georgeLucas:Professional = new Professional("George Lucas",75,"male",90,184,"Black","Brown","Caucasian",true,"Us citizen",1,"Director")
+let jonFavreau:Professional = new Professional("Jon Favreau",54,"male",80,184,"Black","Brown","Caucasian",false,"Us citizen",2,"Director")
+let stanLee:Professional = new Professional("Stan Lee",95,"male",73,180,"White","Blue","Caucasian",false,"Us citizen",1,"writer")
+let robertDowney:Professional = new Professional("Robert Downey Jr",55,"male",74,175,"dark blonde","grey","Caucasian",true,"Us citizen",2,"Actour")
+let carrieFisher:Professional = new Professional("Carrie Fisher",60,"female",74,156,"blonde","brown","Caucasian",false,"Us citizen",2,"Actour")
+let harrisonford:Professional = new Professional("Harrison Ford",78,"male",90,180,"white","brown","caucasian",false,"Us citizen",0,"actour")
+let umaThurman:Professional = new Professional("Uma Thurman", 50, "female",60,183,"blonde","blue","caucasian",false,"Us citizen",0,"actour")
+let dicaprio:Professional = new Professional("Leo Dicaprio",46,"male",80,183,"blonde","blue","caucasian",false,"us citizen",0,"actour")
+let bradPitt:Professional = new Professional("Brad Pitt",57,"male",78,180,"blonde", "blue","Caucasian",true,"Us citizen",2,"actour")
+let penelopeCruz:Professional = new Professional("Penelope Cruz",46,"female",55,165,"brunette","brown","Causian",true,"Spanish",1,"actour")
+let eduardoNoriega:Professional = new Professional("Eduardo Noruega",47,"male",75,179,"brunette","brown","caucasian",false,"spanish",0,"actour")
+let alejandroAmenabar:Professional = new Professional("Alejandro Amenabar",48,"male",65,180,"brown","black","latin",true,"spanish",1,"director")
 jonFavreau.getPrint();
