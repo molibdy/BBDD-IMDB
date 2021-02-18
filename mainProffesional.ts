601c34e34e50a5ce5b19478a554a5ec9cdb9ab53
Fran Martinez <fran.martinez.torres@gmail.com>
2021-02-18 13:28:45 +0100

    pofffffesional malo borrado

--- mainProffesional.ts
+++
@@ -1,7 +0,0 @@
-import {Professional} from "./claseProfessional"
-
-let tarantino:Professional = new Professional("Quentin Tarantino",54,"Accion",80,184,"Black","Brown","Caucasian",false,"Us citizen",2,"Director")
-let georgeLucas:Professional = new Professional("George Lucas",75,"Sci-Fi",90,184,"Black","Brown","Caucasian",true,"Us citizen",1,"Director")
-let jonFavreau:Professional = new Professional("Jon Favreau",54,"Action",80,184,"Black","Brown","Caucasian",false,"Us citizen",2,"Director")
-
-jonFavreau.getPrint();
